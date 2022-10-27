9c83cd9370910dba3b4985fdbbd059a968aab701
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-27 18:14:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Feedback from './Feedback/Feedback'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <Feedback />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
