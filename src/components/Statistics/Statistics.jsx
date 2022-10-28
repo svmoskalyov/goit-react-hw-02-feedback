a45fd7b48c2c8ec87e2c811876dbc98d9e3eef54
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 18:35:01 +0300

    Creates base files for to Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+const Statistics = () => (
+  <>
+    <ul>
+      <li>Good</li>
+      <li>Neutral</li>
+      <li>Bad</li>
+      <li>Total</li>
+      <li>Positive feedback</li>
+    </ul>
+  </>
+);
+
+export default Statistics;
