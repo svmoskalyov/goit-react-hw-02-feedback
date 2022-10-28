1f15231e861959c5ad79fcffe84c0df71d3c822e
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 22:31:23 +0300

    Creates Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,16 @@
-const Section = () => (
-  <section>
-    <h1></h1>
+import PropTypes from 'prop-types';
+import styles from './Section.module.css';
+
+const Section = ({ title, children = [] }) => (
+  <section className={styles.section}>
+    <h1 className={styles.title}>{title}</h1>
+    {children}
   </section>
 );
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
 
 export default Section;
