41af3013c10ab6086e527c54a92f59ff04f2d760
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 18:37:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import Feedback from './Feedback/Feedback'
+import Container from '../components/Container';
+import Section from '../components/Section';
+import FeedbackOptions from '../components/FeedbackOptions';
+import Statistics from '../components/Statistics';
+// import Feedback from './Feedback/Feedback'
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
   render() {
     return (
-      <>
-        <div>
-          <Feedback />
-        </div>
-      </>
+      <Container>
+        <Section>
+          <FeedbackOptions />
+        </Section>
+
+        <Section>
+          <Statistics />
+        </Section>
+      </Container>
     );
   }
 }
 
 export default App;
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <div>
+//           <Feedback />
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
 
 // export const App = () => {
 //   return (
