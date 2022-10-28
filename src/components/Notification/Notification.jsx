a5f630c41fd827d377907c7cb00ad5fdba95cf7c
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 22:32:13 +0300

    Creates Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,3 +1,12 @@
-const Notification = () => <p>message</p>;
+import PropTypes from 'prop-types';
+import styles from './Notification.module.css';
+
+const Notification = ({ message }) => (
+  <p className={styles.notification}>{message}</p>
+);
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Notification;
