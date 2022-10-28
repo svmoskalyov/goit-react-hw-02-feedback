62e4d433c41954ecfcd49646e2fe5517375f1ed6
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 22:31:48 +0300

    Creates FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,29 @@
-const FeedbackOptions = () => {
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const optionNames = Object.keys(options);
+
   return (
-    <ul>
-      <li>
-        <button></button>
-      </li>
+    <ul className={styles.btnList}>
+      {optionNames.map(optionName => (
+        <li className={styles.item} key={optionName}>
+          <button
+            className={styles.button}
+            name={optionName}
+            onClick={() => onLeaveFeedback(optionName)}
+          >
+            {optionName}
+          </button>
+        </li>
+      ))}
     </ul>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.objectOf(PropTypes.number.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
