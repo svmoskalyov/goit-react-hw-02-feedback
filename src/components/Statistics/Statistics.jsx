3c5b0219bec74f4e32c535e771d63f5d388e8996
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-28 22:32:01 +0300

    Creates Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,31 @@
-const Statistics = () => (
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+import Notification from '../Notification';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <>
-    <ul>
-      <li>Good</li>
-      <li>Neutral</li>
-      <li>Bad</li>
-      <li>Total</li>
-      <li>Positive feedback</li>
-    </ul>
+    {total ? (
+      <ul className={styles.list}>
+        <li className={styles.input}>Good: {good}</li>
+        <li className={styles.input}>Neutral: {neutral}</li>
+        <li className={styles.input}>Bad: {bad}</li>
+        <li className={styles.total}>Total: {total}</li>
+        <li className={styles.feedback}>
+          Positive feedback: {positivePercentage}%
+        </li>
+      </ul>
+    ) : (
+      <Notification message="No feedback given" />
+    )}
   </>
 );
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
 export default Statistics;
